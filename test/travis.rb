60859257d8ab186b6359a44ed4de83e034268460
Matt Andrews <matt@mattandre.ws>
2014-02-02 12:52:40 +0000

    Remove redundancy from tests - came up during #11 code review

--- test/travis.rb
+++ test/travis.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-require 'fileutils'
-require 'open3'
+require "fileutils"
+require "open3"
 
-# squish method borrowed from Rails that removes newlines and extra spaces
+# Squish method borrowed from Rails that removes newlines and extra spaces
 class String
 	def squish
-		strip.gsub /\s+/, ' '
+		strip.gsub /\s+/, " "
 	end
 end
 
@@ -13,20 +13,20 @@ end
 FileUtils.mkdir_p "build"
 
 # Compile o-colors normally
-result, status = Open3.capture2e "sass --scss main.scss build/main.css"
+result, status = Open3.capture2e "sass main.scss build/main.css"
 raise result unless status.success?
-raise "When compiled the module should output some CSS" unless File.exists? 'build/main.css'
+raise "When compiled the module should output some CSS" unless File.exists? "build/main.css"
 puts "Checked regular compile"
 
 # Compile o-colors with silent classes
-result, status = Open3.capture2e "sass --scss test/silent.scss build/silent.css --style compressed"
+result, status = Open3.capture2e "sass test/silent.scss build/silent.css --style compressed"
 raise result unless status.success?
-raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size('build/silent.css') == 0
+raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size("build/silent.css") == 0
 puts "Checked silent compile"
 
 # Attempting to use an undefined color generates a warning
-stdout, stderr, status = Open3.capture3 "sass --scss test/undefined-colors-warn.scss build/undefined-colors-warn.css --style compressed"
-raise "Using an undefined color shouldn't fail build" unless status.success?
+stdout, stderr, status = Open3.capture3 "sass test/undefined-colors-warn.scss build/undefined-colors-warn.css --style compressed"
+raise "Using an undefined color should not fail build" unless status.success?
 raise "Using an undefined color should throw a warning" unless stderr.squish.match /^WARNING\: Undefined color used on line [0-9]+ of [0-9A-Za-z_\/\\\.]+$/
 raise "Using an undefined color should not affect output" unless File.open("build/undefined-colors-warn.css").read.squish == ".test{color:#000}"
 puts "Checked non-fatal warnings thrown when undefined color used"
