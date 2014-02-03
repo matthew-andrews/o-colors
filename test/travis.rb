bd52e33608e686b0662ce4529f5c79b10742626b
Matt Andrews <matt@mattandre.ws>
2014-02-03 09:33:50 +0000

    Eliminate the file system

--- test/travis.rb
+++ test/travis.rb
@@ -9,24 +9,21 @@ class String
 	end
 end
 
-# Prepare build test folder
-FileUtils.mkdir_p "build"
-
 # Compile o-colors normally
-result, status = Open3.capture2e "sass main.scss build/main.css"
+result, status = Open3.capture2e "sass main.scss"
 raise result unless status.success?
-raise "When compiled the module should output some CSS" unless File.exists? "build/main.css"
+raise "When compiled the module should output some CSS" unless result.length > 0
 puts "Checked regular compile"
 
 # Compile o-colors with silent classes
-result, status = Open3.capture2e "sass test/silent.scss build/silent.css --style compressed"
+result, status = Open3.capture2e "sass test/silent.scss --style compressed"
 raise result unless status.success?
-raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless File.size("build/silent.css") == 0
+raise "When $o-colors-is-silent is set to true the module should not output any CSS" unless result.length == 0
 puts "Checked silent compile"
 
 # Attempting to use an undefined color generates a warning
-stdout, stderr, status = Open3.capture3 "sass test/undefined-colors-warn.scss build/undefined-colors-warn.css --style compressed"
+stdout, stderr, status = Open3.capture3 "sass test/undefined-colors-warn.scss --style compressed"
 raise "Using an undefined color should not fail build" unless status.success?
 raise "Using an undefined color should throw a warning" unless stderr.squish.match /^WARNING\: Undefined color used on line [0-9]+ of [0-9A-Za-z_\/\\\.]+$/
-raise "Using an undefined color should not affect output" unless File.open("build/undefined-colors-warn.css").read.squish == ".test{color:#000}"
+raise "Using an undefined color should not affect output" unless stdout.squish == ".test{color:#000}"
 puts "Checked non-fatal warnings thrown when undefined color used"
